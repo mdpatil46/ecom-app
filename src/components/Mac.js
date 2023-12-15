dbf0ed1fff5b4ae9f29b48049deffd4fec197fab
Govinda <govindapatil46@gmail.com>
2023-12-15 13:48:57 +0530

    15/12/23 till 1:48

--- src/components/Mac.js
+++ src/components/Mac.js
@@ -2,18 +2,27 @@ import React from 'react'
 
 function Mac() {
 
-
+ setTimeout(() => {
+  document.getElementById('blue').style = 'background:white; color:black;  transition: background-color 1.5s ease;';
+ }, 1000);
 
   return (
     <>
     
-      <div className='bg-gray-100 h-28 w-99 text-black  '>
+      <div className='bg-blue-700 h-20 w-99 text-white flex items-center' id='blue'>
       <div className="mx-80 text-center ">
           <p className="text-xm ">
           Get up to ₹8000.00 instant savings on selected Mac models with eligible HDFC Bank cards. Footnote §§ Plus No Cost EMI from most leading banks.
           </p>
         </div>
       </div>
+
+     <div className="mx-auto flex-wrap justify-center  bg-gray-200 mt-0">
+       <p className='text-7xl font-bold ml-20'>Mac</p>
+       <p className='text-2xl font-semibold '>if you can dream it, mac can do it.</p>
+
+     </div>
+
     </>
   )
 }
