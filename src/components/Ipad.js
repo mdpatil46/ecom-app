c6928a9b31a6b634e0f3dea13ae4ef4d4f2cdc46
Govinda <govindapatil46@gmail.com>
2023-12-24 00:47:21 +0530

    24/12/23
    24/12/23 till 12:47

--- src/components/Ipad.js
+++ src/components/Ipad.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Ipad() {
   return (
     <>
-      
+      <div class="text-blue-500 sm:text-green-500 md:text-yellow-500 lg:text-orange-500 xl:text-red-500 text-5xl font-bold">
+  This text will have different colors on different screen sizes.
+</div>
+
 
     </>
   )
