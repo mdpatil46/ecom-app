23f43dc2ff22c764dcfd105644054022a25bcc1c
Govinda <govindapatil46@gmail.com>
2023-12-24 20:50:10 +0530

    24/12/23 till 8:49 small screen side bar menu button not working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,155 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  
-const[ismenuopen, setmenuopen] = useState(false);
+  const [ismenuopen, setmenuopen] = useState(false);
 
-function changeNav(isLogoClick){
-  let mystyle = document.getElementById("changeNavbar")
-  mystyle.style.color = isLogoClick ? "orange" : 'black'; 
-  mystyle.style.backgroundColor = isLogoClick ? "black" : 'gray';
-
-}
+  function changeNav(isLogoClick) {
+    let mystyle = document.getElementById("changeNavbar");
+    mystyle.style.color = isLogoClick ? "orange" : "black";
+    mystyle.style.backgroundColor = isLogoClick ? "black" : "gray";
+  }
   return (
     <>
-      <nav className="bg-gray-200 text-black opacity-80 p-2 flex flex-wrap justify-between items-center sm:px-6 lg:px-8* border-b-2 border-gray-400" id='changeNavbar'>
-        <Link to="/IphoneIMG" className=" text-white text-2xl font-bold" onClick={changeNav } ><svg height="38" viewBox="0 0 14 44" width="14" xmlns="http://www.w3.org/2000/svg">
-                <path d="m13.0729 17.6825a3.61 3.61 0 0 0 -1.7248 3.0365 3.5132 3.5132 0 0 0 2.1379 3.2223 8.394 8.394 0 0 1 -1.0948 2.2618c-.6816.9812-1.3943 1.9623-2.4787 1.9623s-1.3633-.63-2.613-.63c-1.2187 0-1.6525.6507-2.644.6507s-1.6834-.9089-2.4787-2.0243a9.7842 9.7842 0 0 1 -1.6628-5.2776c0-3.0984 2.014-4.7405 3.9969-4.7405 1.0535 0 1.9314.6919 2.5924.6919.63 0 1.6112-.7333 2.8092-.7333a3.7579 3.7579 0 0 1 3.1604 1.5802zm-3.7284-2.8918a3.5615 3.5615 0 0 0 .8469-2.22 1.5353 1.5353 0 0 0 -.031-.32 3.5686 3.5686 0 0 0 -2.3445 1.2084 3.4629 3.4629 0 0 0 -.8779 2.1585 1.419 1.419 0 0 0 .031.2892 1.19 1.19 0 0 0 .2169.0207 3.0935 3.0935 0 0 0 2.1586-1.1368z"></path></svg></Link>
-        
-          <button className="flex md:hidden text-white focus:outline-none" onClick={()=> {console.log('Button click'); setmenuopen(!ismenuopen); console.log('Is manu open', ismenuopen); }}>
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-           </button>
-        
+      <nav
+        className="bg-gray-200 text-black opacity-80 p-2 flex flex-wrap justify-between items-center sm:px-6 lg:px-8* border-b-2 border-gray-400"
+        id="changeNavbar"
+      >
+        <Link
+          to="/IphoneIMG"
+          className=" text-white text-2xl font-bold"
+          onClick={changeNav}
+        >
+          <svg
+            height="38"
+            viewBox="0 0 14 44"
+            width="14"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="m13.0729 17.6825a3.61 3.61 0 0 0 -1.7248 3.0365 3.5132 3.5132 0 0 0 2.1379 3.2223 8.394 8.394 0 0 1 -1.0948 2.2618c-.6816.9812-1.3943 1.9623-2.4787 1.9623s-1.3633-.63-2.613-.63c-1.2187 0-1.6525.6507-2.644.6507s-1.6834-.9089-2.4787-2.0243a9.7842 9.7842 0 0 1 -1.6628-5.2776c0-3.0984 2.014-4.7405 3.9969-4.7405 1.0535 0 1.9314.6919 2.5924.6919.63 0 1.6112-.7333 2.8092-.7333a3.7579 3.7579 0 0 1 3.1604 1.5802zm-3.7284-2.8918a3.5615 3.5615 0 0 0 .8469-2.22 1.5353 1.5353 0 0 0 -.031-.32 3.5686 3.5686 0 0 0 -2.3445 1.2084 3.4629 3.4629 0 0 0 -.8779 2.1585 1.419 1.419 0 0 0 .031.2892 1.19 1.19 0 0 0 .2169.0207 3.0935 3.0935 0 0 0 2.1586-1.1368z"></path>
+          </svg>
+        </Link>
+
+        <button
+          className="flex md:hidden text-white focus:outline-none"
+          onClick={() => {
+            setmenuopen(!ismenuopen);
+          }}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="#000000"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+          <div className="md:hidden bg-gray-800 text-white h-screen w-1/5 fixed top-0 right-0 overflow-y-auto" onClick={()=>{setmenuopen(!ismenuopen)}}>
+            <div className="p-4">
+              <h1 className="text-2xl font-bold mb-4">Sidebar Title</h1>
+              <ul>
+                <li className="mb-2">
+                  <Link to="/Home" className="  ">
+                    Store
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  <Link to="/Mac" className=" ">
+                    Mac
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  <Link to="/Ipad" className="  ">
+                    iPad
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  <Link to="/Iphone" className="  ">
+                    iPhone
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  {" "}
+                  <Link to="/Watch" className="  ">
+                    Watch
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  <Link to="/Shope" className=" ">
+                    AirPods
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  {" "}
+                  <Link to="/Categories" className="">
+                    Categories
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  <Link to="/Contact" className=" ">
+                    Contact
+                  </Link>
+                </li>
+                <li className="mb-2">
+                  <Link to="/LoginForm" className="  ">
+                    Login
+                  </Link>
+                </li>
+          
+              </ul>
+            </div>
+          </div>
+        </button>
+
         <div className="hidden md:flex flex-grow items-center justify-center space-x-4">
-          <Link to="/Home" className="  ">Store</Link>
-          <Link to="/Mac" className=" ">Mac</Link>
-          <Link to="/Ipad" className="  ">iPad</Link>
-          <Link to="/Iphone" className="  ">iPhone</Link>
-          <Link to="/Watch" className="  ">Watch</Link>
-          <Link to="/Shope" className=" ">AirPods</Link>
-          <Link to="/Categories" className="">Categories</Link>
-          <Link to="/Contact" className=" ">Contact</Link>
-          <Link to="/LoginForm" className=" ">Login</Link>
+          <Link to="/Home" className="  ">
+            Store
+          </Link>
+          <Link to="/Mac" className=" ">
+            Mac
+          </Link>
+          <Link to="/Ipad" className="  ">
+            iPad
+          </Link>
+          <Link to="/Iphone" className="  ">
+            iPhone
+          </Link>
+          <Link to="/Watch" className="  ">
+            Watch
+          </Link>
+          <Link to="/Shope" className=" ">
+            AirPods
+          </Link>
+          <Link to="/Categories" className="">
+            Categories
+          </Link>
+          <Link to="/Contact" className=" ">
+            Contact
+          </Link>
+          <Link to="/LoginForm" className=" ">
+            Login
+          </Link>
         </div>
-      <Link to ="/Addcart" className='md:flex hidden hover:text-2xl font-bold'><svg height="48" viewBox="0 0 17 48" width="17" xmlns="http://www.w3.org/2000/svg">
-                  <path d="m13.4575 16.9268h-1.1353a3.8394 3.8394 0 0 0 -7.6444 0h-1.1353a2.6032 2.6032 0 0 0 -2.6 2.6v8.9232a2.6032 2.6032 0 0 0 2.6 2.6h9.915a2.6032 2.6032 0 0 0 2.6-2.6v-8.9231a2.6032 2.6032 0 0 0 -2.6-2.6001zm-4.9575-2.2768a2.658 2.658 0 0 1 2.6221 2.2764h-5.2442a2.658 2.658 0 0 1 2.6221-2.2764zm6.3574 13.8a1.4014 1.4014 0 0 1 -1.4 1.4h-9.9149a1.4014 1.4014 0 0 1 -1.4-1.4v-8.9231a1.4014 1.4014 0 0 1 1.4-1.4h9.915a1.4014 1.4014 0 0 1 1.4 1.4z"></path></svg></Link>
+        <Link to="/Addcart" className="md:flex hidden hover:text-2xl font-bold">
+          <svg
+            height="48"
+            viewBox="0 0 17 48"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="m13.4575 16.9268h-1.1353a3.8394 3.8394 0 0 0 -7.6444 0h-1.1353a2.6032 2.6032 0 0 0 -2.6 2.6v8.9232a2.6032 2.6032 0 0 0 2.6 2.6h9.915a2.6032 2.6032 0 0 0 2.6-2.6v-8.9231a2.6032 2.6032 0 0 0 -2.6-2.6001zm-4.9575-2.2768a2.658 2.658 0 0 1 2.6221 2.2764h-5.2442a2.658 2.658 0 0 1 2.6221-2.2764zm6.3574 13.8a1.4014 1.4014 0 0 1 -1.4 1.4h-9.9149a1.4014 1.4014 0 0 1 -1.4-1.4v-8.9231a1.4014 1.4014 0 0 1 1.4-1.4h9.915a1.4014 1.4014 0 0 1 1.4 1.4z"></path>
+          </svg>
+        </Link>
       </nav>
     </>
-  )
+  );
 }
 
 export default Navbar;
-
