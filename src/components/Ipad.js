a0f86aabeab504c63e842307143b38d949943100
Govinda <govindapatil46@gmail.com>
2023-12-30 23:11:54 +0530

    29/12/23

--- src/components/Ipad.js
+++ src/components/Ipad.js
@@ -21,11 +21,11 @@ function Ipad() {
 
       <div className=" mx-auto flex-wrap justify-center  bg-gray-200 mt-0">
         <div className=" bg-black ">
-          <div className=" flex justify-end items-end">
-            <div>
-              <p className="text-white text-3xl sm:text-5xl font-extrabold mb-60 ml-32 sm:mb-96 mt-16 sm:ml-10  ">
+          <div className=" flex ">
+            <div className="text-center" >
+              <p className="text-white text-3xl sm:text-5xl font-extrabold   ">
                 iPad Pro{" "}
-                <span className="text-xl text-white font-bold flex  sm:justify-end ml:5 sm:mt-3 ">
+                <span className="text-xl text-white font-bold flex  sm:justify-end   ">
                   {" "}
                   Supercharge by{" "}
                   <img
@@ -33,12 +33,12 @@ function Ipad() {
                     alt=""
                   />{" "}
                 </span>
-                <span className="text-white font-semibold text-xl mt-2 mr-2 sm:mr-4 flex justify-end sm:justify-evenly">
+                <span className="text-white font-semibold text-xl    flex justify-end sm:justify-evenly">
                   From &#8377;81900.00*
                 </span>
                 <button
                   type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  sm:ml-16"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " 
                 >
                   Buy
                 </button>
@@ -54,16 +54,16 @@ function Ipad() {
           </div>
         </div>
 
-        <div className="flex justify-center items-center bg-white">
+        <div className="flex justify-center sm:items-center bg-white">
           <img src="https://www.apple.com/in/ipad/images/overview/hero/ipad_hero__d0tgmaq6shm6_large.jpg" alt="ipad3colr" 
-          className="mt-6"/>
-          <div className="text-center">
-          <p className="text-5xl font-bold ">iPad</p>
-          <p className="text-3xl font-semibold mt-2">Loveble.Drawable.</p>
-          <p className="text-3xl font-semibold ">Magical.</p>
-          <p className="mt-5  font-semibold text-xl">From &#8377;39900.00*</p>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  mt-3">Buy</button>
-          </div>
+          className="ml-48 mt-52 sm:mt-6"/>
+            <div className="text-center ">
+            <p className=" text-3xl sm:text-5xl font-bold ">iPad</p>
+            <p className="text-xl sm:text-3xl font-semibold mt-2">Loveble.Drawable.</p>
+            <p className="text-xl sm:text-3xl font-semibold ">Magical.</p>
+            <p className="mt-5  font-semibold text-lg sm:text-xl">From &#8377;39900.00*</p>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  mt-3">Buy</button>
+            </div>
         </div>
 
       </div>
