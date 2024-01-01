816af24d7dadeb5af19f86124ed8de2c4d49e41d
Govinda <govindapatil46@gmail.com>
2024-01-01 15:53:32 +0530

    till 1/1/24 till 03:53 pm create half iPad component

--- src/components/Ipad.js
+++ src/components/Ipad.js
@@ -21,8 +21,8 @@ function Ipad() {
 
       <div className=" mx-auto flex-wrap justify-center  bg-gray-200 mt-0">
         <div className=" bg-black ">
-          <div className=" flex ">
-            <div className="text-center" >
+          <div className=" flex justify-center items-center ">
+            <div className="text-center ml-6">
               <p className="text-white text-3xl sm:text-5xl font-extrabold   ">
                 iPad Pro{" "}
                 <span className="text-xl text-white font-bold flex  sm:justify-end   ">
@@ -38,7 +38,7 @@ function Ipad() {
                 </span>
                 <button
                   type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " 
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
                 >
                   Buy
                 </button>
@@ -55,17 +55,120 @@ function Ipad() {
         </div>
 
         <div className="flex justify-center sm:items-center bg-white">
-          <img src="https://www.apple.com/in/ipad/images/overview/hero/ipad_hero__d0tgmaq6shm6_large.jpg" alt="ipad3colr" 
-          className="ml-48 mt-52 sm:mt-6"/>
-            <div className="text-center ">
+          <img
+            src="https://www.apple.com/in/ipad/images/overview/hero/ipad_hero__d0tgmaq6shm6_large.jpg"
+            alt="ipad3colr"
+            className="ml-48 mt-52 sm:mt-6"
+          />
+          <div className="text-center  mr-20 ">
             <p className=" text-3xl sm:text-5xl font-bold ">iPad</p>
-            <p className="text-xl sm:text-3xl font-semibold mt-2">Loveble.Drawable.</p>
+            <p className="text-xl sm:text-3xl font-semibold mt-2">
+              Loveble.Drawable.
+            </p>
             <p className="text-xl sm:text-3xl font-semibold ">Magical.</p>
-            <p className="mt-5  font-semibold text-lg sm:text-xl">From &#8377;39900.00*</p>
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  mt-3">Buy</button>
-            </div>
+            <p className="mt-5  font-semibold text-lg sm:text-xl">
+              From &#8377;39900.00*
+            </p>
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  mt-3"
+            >
+              Buy
+            </button>
+          </div>
         </div>
 
+        <div className="bg-white h-full w-auto mt-8 ml-7 mr-7">
+          <div className=" flex justify-center ">
+            <p className="text-black text-5xl font-bold mt-20">
+              Which iPad is right for you?
+            </p>
+          </div>
+          <div className=" flex justify-relative items-end space-x-20 ml-24 mt-32">
+            <div className="text-center ">
+              <img
+                src="https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_pro__erf9x8mw04sy_large.png"
+                alt="iPadPro"
+              />
+              <p className="text-2xl font-bold mt-12">iPad Pro</p>
+
+              <div className="text-center mt-6">
+              <p>The ultimate iPad</p>
+              <p>experience with the most</p>
+              <p>advance technology</p>
+              </div>
+              <div className="mt-8">
+              <p >From &#8377;81900.008</p>
+               </div>
+
+            </div>
+            <div className=" text-center">
+              <img
+                src="https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_air__bxjv33pk6nte_large.png"
+                alt="iPadPro"
+              />
+               <p className="text-2xl font-bold mt-12">iPad Air</p>
+
+               <div className="text-center mt-6">
+               <p>Serious Performance in</p>
+               <p>a thin and light design.</p>
+               </div>
+               <div className="mt-8">
+               <p className="">From &#8377;59900.00*</p>
+               </div>
+
+            </div>
+            <div className="text-center ">
+              <img
+                src="https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_10_9__f7p2wja0gwuy_large.png"
+                alt="iPadPro"
+              />
+               <p className="text-2xl font-bold mt-12">iPad</p>
+
+               <div className="text-center mt-6">
+               <p>The all‑new colourful</p>
+               <p>iPad for the things</p>
+               <p>you do every day.</p>
+               </div>
+               <div className="mt-8">
+               <p className="">From &#8377;39900.00*</p>
+               </div>
+             
+            </div>
+            <div className="text-center">
+              <img
+                src="https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_10_2__fwgwy7jydtea_large.png"
+                alt="iPadPro"
+              />
+               <p className="text-2xl font-bold mt-12">iPad</p>
+
+               <div className="text-center mt-6">
+               <p>All the essentials in the</p>
+               <p>most affordable iPad.</p>
+               </div>
+               <div className="mt-8">
+               <p className="">From &#8377;32900.00*</p>
+               </div>
+               
+            </div>
+            <div className="text-center">
+              <img
+                src="https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_mini__czcsk9ukpeie_large.png"
+                alt="iPadPro"
+              />
+               <p className="text-2xl font-bold mt-12">iPad mini</p>
+
+               <div className="text-center mt-6">
+               <p>The full iPad</p>
+               <p>experience designed</p>
+               <p>to fit in one hand.</p>
+               </div>
+               <div className="mt-8">
+               <p className="">From &#8377;49900.00*</p>
+               </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/IphoneIMG.js
+++ src/components/IphoneIMG.js
@@ -1,26 +1,42 @@
-import React from 'react'
-    
+import React from "react";
+
 export default function IphoneIMG() {
-    
-    return (
-        <>
-            <div className="bg-black  ">
-                <div className="flex  bg-black justify-center items-center ">
-                    <p className='text-white text-3xl font-bold'>iPhone15Pro</p>
-                </div>
-                <div className='flex justify-center items-center h-screen'>
-                    <img src="https://www.apple.com/in/iphone-15-pro/images/overview/welcome/hero_endframe__ov6ewwmbhiqq_large.jpg" alt="IPHONE 15" className='max-auto' />
-                </div>
-                <div className='flex justify-center items-center h-screen'>
-                    <img src="https://www.apple.com/in/iphone-15-pro/images/overview/closer-look/all_colors__eppfcocn9mky_large.jpg" alt="IPHONE 15" className='max-auto' />
-                </div>
-                <div className='flex justify-center items-center h-screen my-10'>
-                    <img src="https://www.apple.com/v/iphone-15-pro/b/images/overview/camera/48mp_camera__cxq25ak34nau_medium.jpg" alt="IPHONE 15" className='max-auto' />
-                </div>
-                <div className='flex justify-center items-center h-screen my-32 '>
-                    <img src="https://www.apple.com/in/iphone-15-pro/images/overview/usb-c/usbc__dqdn0phay4mu_medium.jpg" alt="IPHONE 15" className='max-auto mt-3' />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="bg-black  ">
+        <div className="flex justify-center items-center relative h-screen">
+          <img
+            src="https://www.apple.com/in/iphone-15-pro/images/overview/welcome/hero_endframe__ov6ewwmbhiqq_large.jpg"
+            alt="IPHONE 15"
+            className="max-auto"
+          />
+          <div class="absolute inset-0 flex items-center justify-center">
+            <p class="text-white text-4xl font-bold mb-96">iPhone15Pro</p>
+          </div>
+          
+        </div>
+        <div className="flex justify-center items-center h-screen">
+          <img
+            src="https://www.apple.com/in/iphone-15-pro/images/overview/closer-look/all_colors__eppfcocn9mky_large.jpg"
+            alt="IPHONE 15"
+            className="max-auto"
+          />
+        </div>
+        <div className="flex justify-center items-center h-screen my-10">
+          <img
+            src="https://www.apple.com/v/iphone-15-pro/b/images/overview/camera/48mp_camera__cxq25ak34nau_medium.jpg"
+            alt="IPHONE 15"
+            className="max-auto"
+          />
+        </div>
+        <div className="flex justify-center items-center h-screen my-32 ">
+          <img
+            src="https://www.apple.com/in/iphone-15-pro/images/overview/usb-c/usbc__dqdn0phay4mu_medium.jpg"
+            alt="IPHONE 15"
+            className="max-auto mt-3"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
