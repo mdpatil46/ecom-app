33ca3ecb8ba97cbb125aa1bf953f7fe912db6a52
Govinda <govindapatil46@gmail.com>
2024-01-09 18:44:03 +0530

    09/01/2024 till 6:39 pm complet  iPhone component

--- src/components/Shope.js
+++ src/components/Shope.js
@@ -3,9 +3,65 @@ import React from 'react'
 export default function Shope() {
     return (
         <>
-       <>
-        <h1>Nothing is everything</h1>
-       </>
+      <div className="bg-gray-100 h-14 text-black flex items-center w-full sm:h-20">
+        <div className="mx-2 sm:mx-80 text-center">
+          <p className="text-xs sm:text-base">
+          Save ₹1000.00 instantly on AirPods Pro with HDFC Bank Credit Cards.+ Plus No Cost EMI from most leading banks.
+          </p>
+        </div>
+      </div>
+
+      <div className=" mx-auto flex-wrap justify-center  bg-gray-100 ">
+
+      <div className="flex flex-absolute    text-center ">
+          <div className="h-400 w-400 relative overflow-hidden bg-white rounded-lg m-3 ">
+            <img
+              src="https://www.apple.com/v/airpods/u/images/overview/airpods_3rd_gen__dhy5bknhvtqq_large.jpg"
+              alt=""
+              className="relative block"
+            />
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-2xl font-bold'>
+            <p className="text-6xl">
+            AirPods
+            </p>
+            <p className="text-2xl font-semibold mt-3 ">3rd genration</p>
+            <p className="text-2xl font-semibold  ">From ₹19900.00*</p>
+            </div>
+          </div>
+          </div>
+
+          <div className="flex justify-center items-cenetr    text-center ">
+          <div className="h-400 w-400  overflow-hidden bg-white rounded-lg m-3 flex justify-center items-center relative ">
+            <img
+              src="https://www.apple.com/v/airpods/u/images/overview/airpods_max_logo__bdgdixidcqcy_large.jpg"
+              alt=""
+              className="mt-72 ml-32 mr-32 mb-72 "
+            />
+            <img src="https://www.apple.com/v/airpods/u/images/overview/airpods_max__f265q4g4ddym_large.png" alt="" className='absolute inset-0 mt-28 ml-96 mr-32 mb-44' />
+            <div className='absolute mt-64 text-black text-2xl font-bold'>
+            <p className="text-2xl font-semibold">₹59900.00*</p>
+            </div>
+           
+          </div>
+          </div>
+
+          <div className="mt-32 sm:mt-28 text-center">
+          <p className="text-3xl ml-5 sm:text-6xl font-bold sm:ml-20">
+          A magical connection to
+          </p>
+          <p className="text-3xl ml-5 sm:text-6xl font-bold sm:ml-20">
+          your devices.
+          </p>
+        </div>
+
+
+        <div>
+            
+        </div>
+      
+
+
+      </div>
             
         </>
     )
--- src/components/Watch.js
+++ src/components/Watch.js
@@ -122,7 +122,7 @@ function Watch() {
                 <p>Light on price.</p>
               </div>
               <div className="mt-8">
-                <p>From ₹134900.00*</p>
+                <p>From ₹29900.00*</p>
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5"
@@ -146,7 +146,7 @@ function Watch() {
                 <p>advanced health features.</p>
               </div>
               <div className="mt-8">
-                <p className="">From ₹79900.00*</p>
+                <p className="">From ₹41900.00*</p>
               </div>
               <button
                 type="button"
@@ -166,10 +166,11 @@ function Watch() {
               </p>
 
               <div className="text-center mt-6">
-                <p>As amazing as ever.</p>
+                <p>The most rugged</p>
+                <p>and capable.</p>
               </div>
               <div className="mt-8">
-                <p className="">From ₹69900.00*</p>
+                <p className="">From ₹89900.00*</p>
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5"
@@ -183,6 +184,98 @@ function Watch() {
           </div>
         </div>
 
+
+        
+
+        <div className="flex flex-absolute justify-center space-x-9 mt-12 text-center ml-3 mr-3">
+          <div className="h-400 w-400 relative overflow-hidden bg-gray-100 rounded-lg ml-3 ">
+            <p className="text-3xl font-bold mt-20 ">
+            Why Apple Watch
+            </p>
+            <p className="text-5xl font-bold mt-1 ">It’s the ultimate device</p>
+            <p className="text-5xl font-bold mt-1 ">
+            for a healthy life.
+            </p>
+
+            <img
+              src="https://www.apple.com/in/watch/home/images/overview/why-watch/tile_why_watch__ft68r958qmye_large.jpg"
+              alt=""
+              className="mt-28"
+            />
+          </div>
+
+          <div className="h-400 w-400 relative overflow-hidden bg-gray-100 rounded-lg mr-3 text-center">
+            <p className="text-3xl font-bold mt-20  ">watchOS 10</p>
+            <p className="text-5xl font-bold mt-1  ">Everything you love.</p>
+            <p className="text-5xl font-bold mt-1  ">like never before.</p>
+            
+            <img
+              src="https://www.apple.com/in/watch/home/images/overview/watchos-10/watchos_banner__bfwu1ze5oy0y_large.jpg"
+              alt=""
+              className="ml-1 mr-1 mt-32"
+            />
+          </div>
+        </div>
+
+
+        <div className="flex flex-absolute justify-center space-x-9 mt-12 text-center ml-3 mr-3">
+          <div className="h-400 w-400 relative overflow-hidden bg-gray-100 rounded-lg ml-3 ">
+            <p className="text-4xl font-bold mt-20 ">
+            Health app
+            </p>
+            <p className="text-2xl font-semibold mt-1 ">Your health data. In your control.</p>
+           
+            <img
+              src="https://www.apple.com/in/watch/home/images/overview/health-app/tile_health__exde7a2pwjau_large.jpg"
+              alt=""
+              className="mt-28"
+            />
+          </div>
+          </div>
+
+
+          <div className="ml-5 mr-5  sm:ml-32 sm:mr-32 mt-12">
+          <p>
+
+        
+*Listed pricing is Maximum Retail Price (inclusive of all taxes).
++Instant savings, otherwise referred to as instant cashback, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards and EasyEMI Credit Cards only. Minimum transaction value of ₹10001.00 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Additional terms apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.
+‡No Cost EMI is available with the purchase of an eligible product made using eligible cards on 3- or 6-month tenures from most leading banks. Monthly pricing is rounded to the nearest rupee. Exact pricing will be provided by your bank, subject to your bank’s terms and conditions. Minimum order spend applies as per your card-issuing bank threshold. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Offer may be revised or withdrawn at any time without any prior notice. Additional terms apply.
+Representative example:
+Based on a 6-month tenure. ₹79900.00 total cost includes 15% p.a. and No Cost EMI savings of ₹3380.00, paid over 6 months as six monthly payments of ₹13317.00.
+Learn more at apple.com/in/2030.
+Irregular rhythm notification requires the latest version of watchOS and iOS. It is not intended for use by people under 22 years old or those who have been previously diagnosed with atrial fibrillation (AFib).
+Cycle Tracking app should not be used for birth control or to diagnose a health condition.
+Emergency SOS requires a cellular connection or Wi-Fi calling with an Internet connection from your Apple Watch or nearby iPhone.
+Requires Apple Watch Series 7, Apple Watch SE, Apple Watch Ultra or later. For a list of supported countries and regions, see apple.com/in/watchos/feature-availability.
+Apple Watch SE has a water resistance rating of 50 metres under ISO standard 22810:2010. This means that it may be used for shallow-water activities like swimming in a pool or ocean. Apple Watch SE should not be used for scuba diving, waterskiing or other activities involving high-velocity water or submersion below shallow depth. Water resistance is not a permanent condition and can diminish over time. For additional information, see support.apple.com/en-in/HT205000.
+Wireless service plan is required for cellular service. Contact your service provider for more details. Connection may vary based on network availability. Check apple.com/in/watch/cellular for participating wireless carriers and eligibility. See support.apple.com/en-in/HT207578 for additional setup instructions.
+All-day battery life is based on the following use: 90 time checks, 90 notifications, 45 minutes of app use and a 60-minute workout with music playback from Apple Watch via Bluetooth, over the course of 18 hours; Apple Watch SE (2nd generation) (GPS) usage includes connection to iPhone via Bluetooth during the entire 18-hour test; Apple Watch SE (2nd generation) (GPS + Cellular) usage includes a total of 4 hours of LTE connection and 14 hours of connection to iPhone via Bluetooth over the course of 18 hours. Testing conducted by Apple in August 2022 using pre-production Apple Watch SE (2nd generation) (GPS) and Apple Watch SE (2nd generation) (GPS + Cellular), each paired with an iPhone; all devices tested with pre-release software. Battery life varies by use, configuration, cellular network, signal strength and many other factors; actual results will vary.
+Blood Oxygen app is for wellness purposes only and not for medical use.
+ECG app is available on Apple Watch Series 4 and later (excluding Apple Watch SE), and can generate an ECG similar to a single-lead electrocardiogram. Intended for use by people 22 years old and over.
+The temperature sensing feature is not intended for medical use.
+Apple Watch Series 9 has a water resistance rating of 50 metres under ISO standard 22810:2010. This means that it may be used for shallow-water activities like swimming in a pool or ocean. However, Apple Watch Series 9 should not be used for scuba diving, waterskiing or other activities involving high-velocity water or submersion below shallow depth. Water resistance is not a permanent condition and can diminish over time.
+For additional information see support.apple.com/en-in/HT205000.
+All-day battery life is based on the following use: 90 time checks, 90 notifications, 45 minutes of app use and a 60-minute workout with music playback from Apple Watch via Bluetooth, over the course of 18 hours; Apple Watch Series 9 (GPS) usage includes connection to iPhone via Bluetooth during the entire 18-hour test; Apple Watch Series 9 (GPS + Cellular) usage includes a total of 4 hours of LTE connection and 14 hours of connection to iPhone via Bluetooth over the course of 18 hours. Battery life in Low Power Mode is based on the following use: 180 time checks, 180 notifications, 90 minutes of app use and a 60-minute workout with music playback from Apple Watch via Bluetooth, over the course of 36 hours; Apple Watch Series 9 (GPS) usage includes connection to iPhone via Bluetooth during the entire 36-hour test; Apple Watch Series 9 (GPS + Cellular) usage includes on-demand LTE connection and 28 hours of connection to iPhone via Bluetooth over the course of 36 hours. Testing conducted by Apple in August 2023 using pre-production Apple Watch Series 9 (GPS) and Apple Watch Series 9 (GPS + Cellular), each paired with an iPhone; all devices tested with pre-release software. Battery life varies by use, configuration, cellular network, signal strength and many other factors; actual results will vary.
+Charge times are from 0–80% and 0–100% using the included Apple Watch Magnetic Fast Charging USB-C Cable. Testing conducted by Apple in August 2023 using pre-production Apple Watch Series 9 (GPS) and Apple Watch Series 9 (GPS + Cellular), each paired with an iPhone; all devices tested with pre-release software, Apple Watch Magnetic Fast Charging USB-C Cable (Model A2515) and Apple 20W USB-C Power Adapter (Model A2305). Charge time varies with region, settings and environmental factors; actual results will vary.
+Apple Watch Ultra 2 has a water resistance rating of 100 metres under ISO standard 22810. It may be used for recreational scuba diving (with compatible third-party app from the App Store) to 40 metres and high-speed water sports. Apple Watch Ultra 2 should not be used for diving below 40 metres. Water resistance is not a permanent condition and can diminish over time. For additional information see support.apple.com/en-in/HT205000.
+All-day battery life is based on the following use: 180 time checks, 180 notifications, 90 minutes of app use and a 60-minute workout with music playback from Apple Watch via Bluetooth, over the course of 36 hours; Apple Watch Ultra 2 (GPS + Cellular) usage includes a total of 8 hours of LTE connection and 28 hours of connection to iPhone via Bluetooth over the course of 36 hours. Battery life in Low Power Mode is based on the following use: 360 time checks, 360 notifications, 180 minutes of app use and two 60-minute workouts with music playback from Apple Watch via Bluetooth, over the course of 72 hours; Apple Watch Ultra 2 (GPS + Cellular) usage includes on-demand LTE connection and 60 hours of connection to iPhone via Bluetooth over the course of 72 hours. Testing conducted by Apple in August 2023 using pre-production Apple Watch Ultra 2 (GPS + Cellular) paired with an iPhone; all devices tested with pre-release software. Battery life varies by use, configuration, cellular network, signal strength and many other factors; actual results will vary.
+Charge times are from 0–80% and 0–100% using the included Apple Watch Magnetic Fast Charging USB-C Cable. Testing conducted by Apple in August 2023 using pre-production Apple Watch Ultra 2 (GPS + Cellular) paired with an iPhone; all devices tested with pre-release software, Apple Watch Magnetic Fast Charging USB-C Cable (Model A2515) and Apple 20W USB-C Power Adapter (Model A2305). Charge time varies with region, settings and environmental factors; actual results will vary.
+The Apple One free trial includes only services that you are not currently using through a free trial or a subscription. Plan automatically renews after trial until cancelled. Restrictions and other terms apply.
+New subscribers only. ₹99.00/month after free trial. Plan automatically renews after trial until cancelled.
+Apple Watch Ultra 2, Apple Watch Series 9 and Apple Watch SE require iPhone Xs or later with iOS 17 or later.
+Features are subject to change. Some features, applications and services may not be available in all regions or all languages. View the complete list.
+Straps are subject to availability.
+          </p>
+        </div>
+
+        <div className="h-0.5 w-150 bg-gray-600 ml-5 mr-5 sm:ml-32 sm:mr-32 mt-4"></div>
+
+        <div className="ml-6 mr-6 sm:ml-32 sm:mr-32 mt-1 mb-10">
+          <p>
+            Copyright <sup>@</sup>2023 Apple inc. All right reserved.
+          </p>
+        </div>
 
       </div>
     </>
